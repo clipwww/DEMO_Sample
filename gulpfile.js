92f19a6282fe68ae209909c32cd262b4791b86fb
clipwww <clipwww@gmail.com>
2017-01-02 22:47:52 +0800

    add Gulp 合併壓縮css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var minifyCSS = require('gulp-minify-css');
 var gulpImagemin = require('gulp-imagemin');
+var concat = require('gulp-concat');
+var rename = require("gulp-rename");
 
 //壓縮圖片
 gulp.task('image', function() {
@@ -8,4 +11,18 @@ gulp.task('image', function() {
         .pipe(gulp.dest('./src/images/'));
 });
 
-gulp.task('default', ['image']);
+//合併壓縮css
+gulp.task('minify-css', function() {
+    return gulp.src('src/css/*.css')
+        .pipe(concat('all.css'))
+        .pipe(minifyCSS({
+            keepBreaks: true,
+        }))
+        .pipe(rename(function(path) {
+            path.basename += ".min";
+            path.extname = ".css";
+        }))
+        .pipe(gulp.dest('./src/css/'));
+});
+
+gulp.task('default', ['image', 'minify-css']);
